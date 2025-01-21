b3371e4a541e2953ff03456383a0108e889ffdcb
CriticalFloof <zackbig2005@gmail.com>
2025-01-21 06:57:12 -0500

    Comment Gameloop Plan

--- CritEngine/src/EngineCore/Application.cpp
+++ CritEngine/src/EngineCore/Application.cpp
@@ -14,6 +14,7 @@ namespace Engine {
 
 	void Application::Run()
 	{
+		// We're going to need an initialization method before the loop, and a shutdown method after the loop, as well as some 'update' method for each iteration of the loop.
 		while (true);
 	}
 
