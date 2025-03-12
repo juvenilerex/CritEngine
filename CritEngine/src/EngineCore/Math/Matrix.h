845ff7027267e8e1acff85d2cff45475f7c8438b
CriticalFloof <zackbig2005@gmail.com>
2025-03-12 05:59:32 -0400

    Changed Matrix to a Template Struct

--- CritEngine/src/EngineCore/Math/Matrix.h
+++ CritEngine/src/EngineCore/Math/Matrix.h
@@ -1,53 +1,249 @@
 #pragma once
 
-#include <vector>
 #include "../Core/Base.h"
-
-template class ENGINE_API std::_Vector_val<std::_Simple_types<float>>;
-template class ENGINE_API std::_Compressed_pair<std::allocator<float>, std::_Vector_val<std::_Simple_types<float>>, true>;
-template class ENGINE_API std::vector<float, std::allocator<float>>;
+#include <initializer_list>
 
 namespace Engine {
 
-	struct ENGINE_API Matrix
+	template<typename T, size_t Rows, size_t Columns>
+	struct Matrix
 	{
+		static_assert(std::is_arithmetic<T>::value, "Matrix elements must be of a numeric type!");
 		//Matrix Data is stored in column major order
-		std::vector<float> data;
-		int num_rows, num_columns; 
-		
-		Matrix(int num_rows, int num_columns) : num_columns(num_columns), num_rows(num_rows), data(std::vector<float>(num_columns * num_rows, 0.0)) {};
-		Matrix(int num_rows, int num_columns, std::vector<float> data) : num_columns(num_columns), num_rows(num_rows), data(data) {};
-		Matrix(const Matrix& base) : num_rows(base.num_rows), num_columns(base.num_columns), data(base.data) {};
-		static Matrix Identity(int size);
+		T data[Rows * Columns];
+
+		Matrix() : data{} {};
+
+		Matrix(T data[Rows * Columns]) {
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				this->data[i] = data[i];
+			}
+		};
+
+		Matrix(std::initializer_list<T> data) {
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				this->data[i] = data.begin()[i];
+			}
+		};
+
+		Matrix(const Matrix& base) { 
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				this->data[i] = base.data[i];
+			}
+		};
+
+		template<int R = Rows, int C = Columns>
+		static std::enable_if_t<R == C, Matrix<T, Rows, Columns>> Identity()
+		{
+			Matrix out = Matrix();
+
+			for (int i = 0; i < Rows; i++)
+			{
+				out.data[i * Rows + i] = 1;
+			};
+
+			return out;
+		}
 
 		//Static Math Operations
-		static Matrix Add(const Matrix& first, const Matrix& second);
-		static Matrix Mul(const Matrix& first, const Matrix& second);
 
-		static Matrix Add(const Matrix& base, const float scalar);
-		static Matrix Mul(const Matrix& base, const float scalar);
+		static Matrix<T, Rows, Columns> Add(const Matrix<T, Rows, Columns>& first, const Matrix<T, Rows, Columns>& second)
+		{
+			Matrix out = Matrix();
 
-		static float Determinant(const Matrix& base);
-		static Matrix Transpose(const Matrix& base);
-		static Matrix Inverse(const Matrix& base);
-		
-		static bool IsEqual(const Matrix& first, const Matrix& second);
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				out.data[i] = first.data[i] + second.data[i];
+			}
+
+			return out;
+		}
+
+		template<size_t OtherColumns>
+		static Matrix<T, Rows, OtherColumns> Mul(const Matrix<T, Rows, Columns>& first, const Matrix<T, Columns, OtherColumns>& second)
+		{
+			// Naive implementation
+			Matrix<T, Rows, OtherColumns> out = Matrix<T, Rows, OtherColumns>();
+
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < OtherColumns; j++)
+				{
+					out.data[j * Rows + i] = 0;
+					for (int k = 0; k < Columns; k++)
+					{
+						out.data[j * Rows + i] += first.data[k * Rows + i] * second.data[j * Columns + k];
+					}
+				}
+			}
+
+			return out;
+		}
+
+		static Matrix<T, Rows, Columns> Add(const Matrix<T, Rows, Columns>& first, const T scalar)
+		{
+			Matrix out = Matrix();
+
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				out.data[i] = first.data[i] + scalar;
+			}
+
+			return out;
+		}
+
+		static Matrix<T, Rows, Columns> Mul(const Matrix& base, const T scalar)
+		{
+			Matrix out = Matrix();
+
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				out.data[i] = base.data[i] * scalar;
+			}
+
+			return out;
+		}
+
+		static Matrix<T, Columns, Rows> Transpose(const Matrix<T, Rows, Columns>& base)
+		{
+			Matrix out = Matrix<T, Columns, Rows>();
+
+			for (int i = 0; i < Columns; i++)
+			{
+				for (int j = 0; j < Rows; j++)
+				{
+					out.data[i * Rows + j] = base.data[j * Columns + i];
+				}
+			}
+
+			return out;
+		}
+
+		template<int R = Rows, int C = Columns>
+		static std::enable_if_t<R == C, T> Determinant(const Matrix<T, Rows, Columns>& base)
+		{
+
+			Matrix m = Matrix(base);
+
+			T det = 1.0;
+			T temp;
+			for (int i = 0; i < Rows; i++)
+			{
+				int pivot = i;
+				for (int j = i + 1; j < Rows; j++)
+				{
+					if (abs(m.data[j * Rows + i]) > abs(m.data[pivot * Rows + i]))
+					{
+						pivot = j;
+					}
+				}
+				if (pivot != i)
+				{
+					for (int k = 0; k < Rows; k++)
+					{
+						temp = m.data[i * Rows + k];
+						m.data[i * Rows + k] = m.data[pivot * Rows + k];
+						m.data[pivot * Rows + k] = temp;
+					}
+					det *= -1;
+				}
+				if (m.data[i * Rows + i] == 0)
+				{
+					return 0;
+				}
+				det *= m.data[i * Rows + i];
+				for (int j = i + 1; j < Rows; j++)
+				{
+					T factor = m.data[j * Rows + i] / m.data[i * Rows + i];
+					for (int k = i + 1; k < Rows; k++)
+					{
+						m.data[j * Rows + k] -= factor * m.data[i * Rows + k];
+					}
+				}
+			}
+			return det;
+		}
+
+		template<int R = Rows, int C = Columns>
+		static std::enable_if_t<R == C, Matrix<T, Rows, Columns>> Inverse(const Matrix& base)
+		{
+
+			Matrix input = Matrix(base);
+			Matrix inverse = Matrix::Identity();
+
+			for (int i = 0; i < Rows; i++)
+			{
+				T pivot = input.data[i * Rows + i];
+				ASSERT(pivot != 0, "Inverse matrix doesn't exist!");
+
+				for (int j = 0; j < Rows; j++)
+				{
+					input.data[i * Rows + j] /= pivot;
+					inverse.data[i * Rows + j] /= pivot;
+				}
+
+				for (int j = 0; j < Rows; j++)
+				{
+					if (i != j)
+					{
+						T scale = input.data[j * Rows + i];
+						for (int k = 0; k < Rows; k++)
+						{
+							input.data[j * Rows + k] -= scale * input.data[i * Rows + k];
+							inverse.data[j * Rows + k] -= scale * inverse.data[i * Rows + k];
+						}
+					}
+				}
+			}
+
+			return inverse;
+		}
+
+
+
+		static bool Matrix::IsEqual(const Matrix<T, Rows, Columns>& first, const Matrix<T, Rows, Columns>& second)
+		{
+			for (int i = 0; i < Rows * Columns; i++)
+			{
+				if (first.data[i] != second.data[i]) return false;
+			}
+			return true;
+		}
 
 		//Object Math Operations
 		Matrix Add(Matrix& other) const { return Matrix::Add(*this, other); }
 		Matrix Mul(Matrix& other) const { return Matrix::Mul(*this, other); }
 
-		Matrix Add(const float scalar) const { return Matrix::Add(*this, scalar); }
-		Matrix Mul(const float scalar) const { return Matrix::Mul(*this, scalar); }
+		Matrix Add(const T scalar) const { return Matrix::Add(*this, scalar); }
+		Matrix Mul(const T scalar) const { return Matrix::Mul(*this, scalar); }
 
 		Matrix Transpose() const { return Matrix::Transpose(*this); }
-		float Determinant() const { return Matrix::Determinant(*this); }
-		Matrix Inverse() const { return Matrix::Inverse(*this); }
+		template<int R = Rows, int C = Columns>
+		std::enable_if_t<R == C, T> Determinant() const { return Matrix::Determinant(*this); }
+		template<int R = Rows, int C = Columns>
+		std::enable_if_t<R == C, Matrix<T, Rows, Columns>> Inverse() const { return Matrix::Inverse(*this); }
 
 		bool IsEqual(Matrix& other) const { return Matrix::IsEqual(*this, other); }
 
-		float operator[](int index) { return this->data[index]; }
-		
+		template<size_t OtherColumns>
+		Matrix<T, Rows, OtherColumns> operator*(const Matrix<T, Columns, OtherColumns>& other) { return Matrix::Mul(*this, other); }
 	};
 
+	
+	using Matrix2f = Matrix<float, 2, 2>;
+	template struct ENGINE_API Matrix<float, 2, 2>;
+	using Matrix3f = Matrix<float, 3, 3>;
+	template struct ENGINE_API Matrix<float, 3, 3>;
+	using Matrix4f = Matrix<float, 4, 4>;
+	template struct ENGINE_API Matrix<float, 4, 4>;
+
+	using Matrix2d = Matrix<double, 2, 2>;
+	template struct ENGINE_API Matrix<double, 2, 2>;
+	using Matrix3d = Matrix<double, 3, 3>;
+	template struct ENGINE_API Matrix<double, 3, 3>;
+	using Matrix4d = Matrix<double, 4, 4>;
+	template struct ENGINE_API Matrix<double, 4, 4>;
 }
