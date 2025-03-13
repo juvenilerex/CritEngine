aa0c110db57d06029b135f3369a7229c2f83ab33
JuvenileRex <57381911+juvenilerex@users.noreply.github.com>
2025-03-13 09:01:30 -0500

    a

--- premake5.lua
+++ premake5.lua
@@ -61,3 +61,55 @@ project "CritEngine"
 	include "CritEngine/thirdparty/libs/glad.lua"
 	include "CritEngine/thirdparty/libs/glm.lua"
 	include "CritEngine/thirdparty/libs/imgui.lua"
+
+project "EndlessRunner"
+	location "EndlessRunner"
+	kind "ConsoleApp"
+	language "C++"
+	
+
+	targetdir ("bin/" .. outputDirectory .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputDirectory .. "/%{prj.name}")
+	
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+
+
+	includedirs 
+	{
+		"%{wks.location}/CritEngine/src",
+		"CritEngine/thirdparty/libs/glm/"
+	}
+	
+	links 
+	{
+		"CritEngine", "GLM"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "Off"
+		systemversion "latest"
+
+		defines 
+		{
+			"CE_PLATFORM_WINDOWS"
+		}
+
+
+	filter "configurations:Debug"
+		defines "CE_DEBUG"
+		symbols "On"
+
+	filter "configurations:ReleaseDev"
+		defines "CE_RELEASEDEV"
+		optimize "On"
+
+	filter "configurations:Release"
+		defines "CE_RELEASE"
+		optimize "On"
