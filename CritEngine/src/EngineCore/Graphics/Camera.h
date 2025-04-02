f715f2a42223a0dff42806625e8619cf5df34041
CriticalFloof <zackbig2005@gmail.com>
2025-04-01 22:03:53 -0400

    Added Orthographic Camera

--- CritEngine/src/EngineCore/Graphics/Camera.h
+++ CritEngine/src/EngineCore/Graphics/Camera.h
@@ -32,4 +32,30 @@ namespace Engine {
 		float farPlane;
 	};		  
 
+	class ENGINE_API OrthographicCamera
+	{
+	public:
+
+		OrthographicCamera(float verticalFOV, float aspectRatio, float nearPlane, float farPlane, Vector3 position = Vector3(0, 0, 0), Quaternion rotation = Quaternion());
+
+		Matrix4f GetViewPerspectiveMatrix();
+		Matrix4f GetPerspectiveMatrix();
+		Matrix4f GetViewMatrix();
+
+		void CalculateViewMatrix();
+		void CalculatePerspectiveMatrix();
+
+	private:
+		Matrix4f viewMatrix = Matrix4f::Identity();
+		Matrix4f perspectiveMatrix = Matrix4f::Identity();
+		Matrix4f viewPerspectiveMatrix = Matrix4f::Identity();
+
+		Quaternion rotation;
+		Vector3 position;
+		float height;
+		float aspectRatio;
+		float nearPlane;
+		float farPlane;
+	};
+
 }
