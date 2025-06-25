54271b94bc2da9eb5c09a3231479e9dcb025550f
JuvenileRex <57381911+juvenilerex@users.noreply.github.com>
2025-06-25 03:10:29 -0500

    Build script directory hotfix

--- Binaries/DotNET/CritBuildTool/build_workspace_vs2022-MSVC.bat
+++ Binaries/DotNET/CritBuildTool/build_workspace_vs2022-MSVC.bat
@@ -1,6 +1,5 @@
 @echo off
 echo Generating Visual Studio 2022 Project Files...
-pushd %~dp0\..\
-call .\Source\Programs\CritBuildTool\bin\Debug\net8.0\CritBuildTool.exe vs2022 msc-v143
+call bin\Debug\net8.0\CritBuildTool.exe vs2022 msc-v143
 popd
 pause
